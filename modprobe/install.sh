9e65a30ce946e7f6b3547dcaa8b45da8fd54ef25
imWillX <imWillX@users.noreply.github.com>
2019-02-26 22:40:31 -0800

    add nobeep

---
+++ modprobe/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bash
+
+. ./packages.sh
+
+echo "Updating packages..."
+sudo pacman -Syu --needed --noconfirm
+
+for pkg in "${PKG[@]}"
+do
+    echo "Installing ${pkg}..."
+    sudo pacman -Syu "$pkg" --needed --noconfirm
+done
+
+for aur in "${AUR[@]}"
+do
+    echo "Installing ${aur}..."
+    sudo yaourt -Syu "$aur" --needed --noconfirm
+done
+
+SAVED_DIR=$PWD
+
+cd ~/.dotfiles
+
+dirs=$(find . -maxdepth 1 -mindepth 1 -type d -not -name '.git' -print)
+for dir in $dirs
+do
+  echo "Installing ${dir}..."
+  sh "$dir"/install.sh
+done
+
+cd "$SAVED_DIR"
