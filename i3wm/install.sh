db3013172dcdfc3667b8a13ee6479a42bb33e181
imWillX <imWillX@users.noreply.github.com>
2020-02-17 23:20:48 -0800

    Add blocks

--- i3wm/install.sh
+++ i3wm/install.sh
@@ -1,3 +1,6 @@
 ln -sfT ~/.dotfiles/i3wm/config ~/.i3/config
 #ln -sfT ~/.dotfiles/i3wm/i3status.conf ~/.i3status.conf
 ln -sfT ~/.dotfiles/i3wm/i3blocks.conf ~/.i3blocks.conf
+ln -sfT ~/.dotfiles/i3wm/blocks/memory ~/.config/i3blocks/memory
+ln -sfT ~/.dotfiles/i3wm/blocks/load_avg ~/.config/i3blocks/load_avg
+ln -sfT ~/.dotfiles/i3wm/blocks/battery ~/.config/i3blocks/battery
